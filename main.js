f07ee77e90b19a5633163990dfd4ad42986451a9
joanferrecid098 <6hpd15h0-github@tigelcid.es>
2024-06-02 21:31:36 +0200

    Basic Logic

---
+++ main.js
@@ -0,0 +1,69 @@
+const { chromium } = require('playwright');
+
+/* Changable variables */
+var mission = "Franklin_and_Lamar";
+
+/* Line parsing */
+const parseLine = (text) => {
+    const lineParts = text.split(':');
+    const character = lineParts[0];
+
+    lineParts.shift();
+    const line = lineParts.join(':').substr(1);
+
+    return {
+        character,
+        line
+    }
+}
+
+/* Web scriping */
+(async() => {
+
+    let title = "";
+    let lines = [];
+
+    const browser = await chromium.launch({
+        headless: true,
+    });
+    
+    const context = await browser.newContext();
+
+    const page = await context.newPage();
+    await page.goto(`https://www.grandtheftwiki.com/${mission}/Script`)
+
+    const getTitle = await page.evaluate(() => {
+        const item = document.querySelector('.mw-page-title-main').innerHTML;
+
+        return item;
+    });
+
+    title = getTitle.split("/")[0];
+
+    const getLines = await page.evaluate(() => {
+        let lines = [];
+
+        const items = document.querySelectorAll('.mw-parser-output p');
+
+        for (let item of items) {
+            if (!item.innerHTML.startsWith("("))
+
+            lines.push(item.innerHTML
+                .replace("<b>", "")
+                .replace("</b>", "")
+                .replace("\n", ""));
+        }
+
+        return lines;
+    });
+    
+    getLines.shift();
+    for (let line of getLines) {
+        lines.push(parseLine(line));
+    }
+
+    console.log(title);
+    console.log(lines);
+
+})();
+
